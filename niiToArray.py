00be5b55191232d8bf52fbaab99b183b7b926733
rayhanrazzak <rrazzak07@gmail.com>
2019-06-29 00:55:29 -0700

    got rid of unnecessary stuff from ex file

--- niiToArray.py
+++ niiToArray.py
@@ -3,12 +3,8 @@ import os
 import tensorflow as tf
 import numpy as np
 import SimpleITK as sitk
-#print(np.eye(2))
+#print(os.getcwd())
 file = '/Users/Flash/Desktop/Caltech_51456/SCANS/anat/NIfTI/mprage.nii'
 test_image = sitk.ReadImage(file)
-
-
 test_array = sitk.GetArrayFromImage(test_image)
 print(test_array)
-#os.chdir('/Desktop')
-#print(os.getcwd())
