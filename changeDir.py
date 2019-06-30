31e990b18e9697cb38f782e3f6f2e583b3f276b4
fencingmaster123 <47678247+fencingmaster123@users.noreply.github.com>
2019-06-30 13:57:42 -0700

    moved functioanl MRI dirs

--- changeDir.py
+++ changeDir.py
@@ -6,8 +6,8 @@ os.chdir('D:')
 #print(file_dir)
 
 
-def oneFolderNewDir(file_dir,autism_true): 
-    k = 0
+def oneFolderNewDir(file_dir,autism_true, anat_true):
+    k = 129
     for i in os.listdir(file_dir):
         if not i.startswith('.'):
             #print(i)
@@ -21,15 +21,30 @@ def oneFolderNewDir(file_dir,autism_true):
                 #os.chdir("D:/ABIDE 1/anatomical/autism positive/KKI_50791/KKI_50791/anat/NIfTI")
                 #for l in os.listdir(os.curdir):
                 #    print(l)
-                file_path = '{}/anat/NIfTI/mprage.nii.gz'.format(directory)
+
+                if(anat_true == 'true'):
+                    file_path = '{}/anat/NIfTI/rest_1.nii.gz'.format(directory)
+                if(anat_true == 'false'):
+                    file_path = '{}/rest/NIfTI/rest.nii.gz'.format(directory)
                 #print(file_path)
                 #new_path = 'D:/ABIDE 1/anatomical/nii files of autism positive/mprage{}.nii.gz'.format(k)
                 #print(new_path)
                 k = k + 1
-                if(autism_true == 'true'):
-                    os.rename('{}'.format(file_path),'D:/ABIDE 1/anatomical/nii files of autism positive/mprage{}.nii.gz'.format(k))
-                if(autism_true == 'false'):
-                    os.rename('{}'.format(file_path), "D:/ABIDE 1/anatomical/nii files of controls/mprage{}.nii.gz".format(k))
+                if(anat_true == 'true'):
+                    if(autism_true == 'true'):
 
-#oneFolderNewDir('D:/ABIDE 1/anatomical/autism positive', 'true')
-#oneFolderNewDir('D:/ABIDE 1/anatomical/controls', 'false')
+                        os.rename('{}'.format(file_path),'D:/ABIDE 1/raw MRIs (functional and anatomical)/ABIDE/anatomical/nii files of autism positive/mprage{}.nii.gz'.format(k))
+                    if(autism_true == 'false'):
+                        os.rename('{}'.format(file_path), "D:/ABIDE 1/raw MRIs (functional and anatomical)/ABIDE/anatomical/nii files of controls/mprage{}.nii.gz".format(k))
+                if(anat_true == 'false'):
+                    if(autism_true == 'true'):
+
+                        os.rename('{}'.format(file_path),'D:/ABIDE 1/raw MRIs (functional and anatomical)/ABIDE/functional/nii files of autism positive/mprage{}.nii.gz'.format(k))
+                    if(autism_true == 'false'):
+                        os.rename('{}'.format(file_path), "D:/ABIDE 1/raw MRIs (functional and anatomical)/ABIDE/functional/nii files of controls/mprage{}.nii.gz".format(k))
+
+
+#oneFolderNewDir('D:/ABIDE 1/anatomical/autism positive', 'true', 'true')
+#oneFolderNewDir('D:/ABIDE 1/anatomical/controls', 'false', 'true')
+#oneFolderNewDir('D:/ABIDE 1/anatomical/controls', 'false', 'false')
+oneFolderNewDir('D:/ABIDE 1/anatomical/autism positive', 'true', 'false')
