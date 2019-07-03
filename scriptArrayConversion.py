94447b7687694a6150417dd545aa830dd29a2bf3
rayhanrazzak <rrazzak07@gmail.com>
2019-07-02 22:24:50 -0700

    added script to find dimensions of each anatomical MRI file

--- scriptArrayConversion.py
+++ scriptArrayConversion.py
@@ -2,7 +2,15 @@ import SimpleITK as sitk
 import numpy as np
 
 
-anat_dir = ""
-for i in anat_dir:
-    readable_image = sitk.ReadImage(i)
-    single_array = sitk.GetArrayFromImage(readable_image)
+anat_dir = 'D:/ABIDE 1/raw MRIs (functional and anatomical)/ABIDE/anatomical/nii files of autism positive'
+control_dir = 'D:/ABIDE 1/raw MRIs (functional and anatomical)/ABIDE/anatomical/nii files of controls'
+
+
+def MRI_dimensions(file_dir):
+    for i in os.listdir(file_dir):
+        readable_image = sitk.ReadImage(i)
+        single_array = sitk.GetArrayFromImage(readable_image)
+        print(np.shape(single_array))
+
+MRI_dimensions(anat_dir)
+MRI_dimensions(control_dir)
