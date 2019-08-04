05b4bd65da65daef892045d3f0a3ee66e07df13c
Rayhan Razzak <rayhanrazzak@Rayhans-MacBook-Pro.local>
2019-08-04 00:42:35 -0700

    fixed pytorch label creator

--- label.py
+++ label.py
@@ -1,7 +1,12 @@
-import numpy as np
-class labeller():
-    def __init__(self):
-        self.label = np.zeros(2).reshape(1,2)
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+data = ImageFolder(root='main_dir', transform=ToTensor())
 
-
-print(np.zeros(2).reshape(1,2))
+print(data.classes)
+x,y = data[0]
+from torch.utils.data import DataLoader
+loader = DataLoader(data)
+#loader = DataLoader(data, shuffle=True)
+for x, y in loader:
+    print(x) # image
+    print(y) # image label
