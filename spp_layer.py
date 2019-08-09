eb417f05c41c057d5d5039cc281b3ab59c26b096
Rayhan Razzak <rayhanrazzak@Rayhans-MacBook-Pro.local>
2019-08-08 17:28:00 -0700

    modified spp_layer.py for 3D format

--- spp_layer.py
+++ spp_layer.py
@@ -1,21 +1,25 @@
 import math
+
+# need to make this work for 3D max pooling
 def spatial_pyramid_pool(self,previous_conv, num_sample, previous_conv_size, out_pool_size):
     '''
     previous_conv: a tensor vector of previous convolution layer
     num_sample: an int number of image in the batch
-    previous_conv_size: an int vector [height, width] of the matrix features size of previous convolution layer
+    previous_conv_size: an int vector [height, width, z-dimension] of the matrix features size of previous convolution layer
     out_pool_size: a int vector of expected output size of max pooling layer
-    
+
     returns: a tensor vector with shape [1 x n] is the concentration of multi-level pooling
-    '''    
+    '''
     # print(previous_conv.size())
     for i in range(len(out_pool_size)):
         # print(previous_conv_size)
         h_wid = int(math.ceil(previous_conv_size[0] / out_pool_size[i]))
         w_wid = int(math.ceil(previous_conv_size[1] / out_pool_size[i]))
+        z_wid = int(math.ceil(previous_conv_size[2] / out_pool_size[i]))
         h_pad = (h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2
         w_pad = (w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2
-        maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
+        z_pad = (z_wid*out_pool_size[i] - previous_conv_size[2] + 1)/2
+        maxpool = nn.MaxPool3d((h_wid, w_wid, z_wid), stride=(h_wid, w_wid, z_wid), padding=(h_pad, w_pad, z_pad))
         x = maxpool(previous_conv)
         if(i == 0):
             spp = x.view(num_sample,-1)
