d012c39669a65aa5134e31ecf9a9192997754659
rayhanrazzak <rrazzak07@gmail.com>
2019-11-09 09:49:16 -0800

    deleted spp_layer found online and added custom spp layer using GCD

---
+++ spp.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+import SimpleITK as sitk
+import nibabel as nib
+from fractions import gcd
+from functools import reduce
+
+#spatial_pyramid_pooling (spp) layer that uses greatest common divisors (GCD) to prevent padding
+class spp:
+    def __init__(self, x):
+        self.x = x #tensor attribute
+        self.size = np.shape(self.x) #size attribute
+        self.a = [] #GCF of first dimension in self.path
+        self.b = [] #GCF of second dimension in self.path
+        self.c = [] #GCF of third dimension in self.path
+    def pool(self, gcd): #MaxPools the tensor
+        self.gcd = gcd
+        self.out = nn.MaxPool3d((self.a,self.b,self.c), padding=False)
+        return self.out
+    def gcd(self, path): #generates the GCD of 3 dimensions for a path
+        self.path = path
+        for i in os.listdir(self.path):
+            self.file = '{}/{}'.format(self.path, i)
+            self.readImage = sitk.ReadImage(self.file)
+            self.array = sitk.GetArrayFromImage(self.readImage)
+            
+            self.a.append(np.shape(self.array)[0]) #list of all files first dimension
+            self.b.append(np.shape(self.array)[1])
+            self.c.append(np.shape(self.array)[2])
+        self.a_gcd = reduce(gcd, self.a)
+        self.b_gcd = reduce(gcd, self.b)
+        self.c_gcd = reduce(gcd, self.c)
+
+        self.gcd = (self.a_gcd, self.b_gcd, self.c_gcd)
+        return self.gcd #returns a tuple
+
+''' How to run:
+ 
+ Requirements:
+    1. x is a tensor
+    2. path is the directory of all MRIs
+ 
+ spp_layer = spp(x) #creating instance of spp layer
+ gcd = spp.gcd(path) #generating the GCD of each dimension of files in path
+ spp_layer = spp.pool(gcd) #returning maxpool
+
+'''
